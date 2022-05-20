0e631b6e5dff5e799fa3638692962bbcefa63769
Shrung Bhatt <shrungbhatt@gmail.com>
2022-05-20 15:27:10 -0400

    Updated the EventChannel.kt

--- app/src/main/java/com/shrungbhatt/carfaxassignment/util/EventChannel.kt
+++ app/src/main/java/com/shrungbhatt/carfaxassignment/util/EventChannel.kt
@@ -1,29 +1,24 @@
 package com.shrungbhatt.carfaxassignment.util
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
 
-class EventChannel: IEventChannel {
+class EventChannel : IEventChannel {
 
     private val channel = Channel<Event>()
 
     override val eventFlow: Flow<Event> = channel.receiveAsFlow()
 
-    override fun emitEvent(type: EventType, message: String) {
-        CoroutineScope(Default).launch {
-            channel.send(Event(type, message))
-        }
+    override suspend fun emitEvent(type: EventType, message: String) {
+        channel.send(Event(type, message))
     }
 }
 
-interface IEventChannel{
+interface IEventChannel {
     val eventFlow: Flow<Event>
 
-    fun emitEvent(type: EventType, message: String)
+    suspend fun emitEvent(type: EventType, message: String)
 }
 
 data class Event(
@@ -31,7 +26,7 @@ data class Event(
     val message: String
 )
 
-enum class EventType{
+enum class EventType {
     ERROR,
     INVALID_DATA
 }
